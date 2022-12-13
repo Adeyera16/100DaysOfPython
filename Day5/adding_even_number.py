b5f30083300ff296c0b45209e13dd6e04e72c8ff
Adeyera <asimolowoelizabeth@gmail.com>
2022-12-13 15:24:50 +0100

    Password generator

---
+++ Day5/adding_even_number.py
@@ -0,0 +1,7 @@
+#create a variable with 0 as its value
+total = 0
+#use the range function to get the numbers 2 - 100
+for number in range(2, 102, 2):
+#add it all together
+    total += number
+print(total)
